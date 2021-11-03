48d23767eb19edc5e0856c3f6bbeed6a6acf6140
Matt R <mattrayner14@gmail.com>
2021-11-03 01:42:14 +0000

    added CRUD operations, added web forms for CRUD operations, added validation, added workaround for UPDATE and DELETE operations

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const {Restaurant, Menu, MenuItem} = require('./db/models.js')
+const methodOverride = require('method-override');
 
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const app = express();
 const port = 3001;
@@ -8,8 +10,9 @@ const port = 3001;
 app.use(express.static('public'));
 
 app.use(express.urlencoded( {extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
-//app.get are endpoints, which return dynamic content
 app.get('/now', (req, res) => {
     const date = new Date();
     res.send(date.toString());
@@ -32,14 +35,16 @@ app.get('/user', function(req, res) {
 res.send(JSON.stringify(response));
 })
 
-app.get('/restaurants', function(req, res) {
+
+//RESTAURANT CRUD
+app.get('/restaurants', (req, res) => {
     Restaurant.findAll()
         .then(data => {
             res.send(data)
         })
 })
 
-app.get('/restaurants/:id', function(req, res) {
+app.get('/restaurants/:id', (req, res) => {
     Restaurant.findByPk(req.params.id, {
         include: {
             model: Menu,
@@ -53,6 +58,59 @@ app.get('/restaurants/:id', function(req, res) {
             res.send(data)
         })
 })
+
+app.post(
+    '/restaurants',
+    //name must be alphanumeric and less than 50 characters
+    check('name').isAlphanumeric(),
+    check('name').isByteLength({min:0, max:50}),
+    //image url must be valid url
+    check('image').isURL(),
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            res.sendStatus(403);
+        } else {
+            Restaurant.create( {name: req.body.name, image: req.body.image});
+            res.sendStatus(201);
+        }
+    }
+)
+
+app.delete(
+    '/restaurants',
+    check('name').isAlphanumeric(),
+    check('name').isByteLength({min:0, max:50}),
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            res.sendStatus(403);
+        } else {
+            Restaurant.destroy({where: {name:req.body.name}});
+            res.sendStatus(202);
+        }
+    }
+)
+
+app.put(
+    '/restaurants',
+    //name must be alphanumeric and less than 50 characters
+    check('name').isAlphanumeric(),
+    check('name').isByteLength({min:0, max:50}),
+    //image url must be valid url
+    check('image').isURL(),
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            res.sendStatus(403);
+        } else {Restaurant.update(
+                {name: req.body.name, image: req.body.image},
+                {where: {id: req.body.id}}
+            )
+            res.sendStatus(202);
+        }
+    }
+)
 
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
